b0e162b4a4a1738de74f35a13725373d33a7696a
prashant022-cmd <143316595+prashant022-cmd@users.noreply.github.com>
2024-09-12 02:05:30 +0530

    Add files via upload
    add

---
+++ App.jsx
@@ -0,0 +1,123 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='gap-10 flex flex-col '>
+
+    <div className='flex items-center justify-center w-full overflow-visible mt-2'>
+      <div className='flex flex-col bg-pink-300 rounded-lg p-4 hover:bg-indigo-300'>
+        <div className='relative w-full h-64 overflow-hidden rounded-lg'>
+          <img
+            src="https://images.pexels.com/photos/28207225/pexels-photo-28207225.jpeg" // Direct image URL
+            title='img'
+            alt="My first project"
+            className='w-full h-full object-cover'
+            />
+        </div>
+        <div className='mt-4 text-center'>
+          <div className='text-black text-lg font-bold underline font-serif'>
+            Home Delivery Available💦
+          </div>
+          <div className='mt-2 font-bold text-sm text-black font-sans '>
+            New And fresh service 300 rupee
+          </div>
+        </div>
+        <div className='flex bg-white h-8 w-28 justify-center ml-24 mt-4 hover:bg-pink-200 rounded-lg hover:w-32 hover:h-10'>
+          <button className="flex items-center font-serif">
+            BOOK NOW
+          </button>
+        </div>
+          <div className='flex font-serif flex-col left-5 '>
+          <text className='flex underline ml-[163px] mt-4'>Contact-8463878715</text>
+          </div>
+      </div>
+    </div>
+    <div className='flex items-center justify-center w-full overflow-visible mt-2'>
+      <div className='flex flex-col bg-pink-300 rounded-lg p-4 hover:bg-indigo-300'>
+        <div className='relative w-full h-64 overflow-hidden rounded-lg'>
+          <img
+            src="https://images.pexels.com/photos/17950882/pexels-photo-17950882/free-photo-of-lorrany-moda-praia.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" // Direct image URL
+            title='img'
+            alt="My first project"
+            className='w-full h-full object-cover'
+            />
+        </div>
+        <div className='mt-4 text-center'>
+          <div className='text-black text-lg font-bold underline font-serif'>
+            Home Delivery Available💦
+          </div>
+          <div className='mt-2 font-bold text-sm text-black font-sans '>
+            New And fresh service 200 rupee
+          </div>
+        </div>
+        <div className='flex bg-white h-8 w-28 justify-center ml-24 mt-4 hover:bg-pink-200 rounded-lg hover:w-32 hover:h-10'>
+          <button className="flex items-center font-serif">
+            BOOK NOW
+          </button>
+        </div>
+          <div className='flex font-serif flex-col left-5 '>
+          <text className='flex underline ml-[163px] mt-4'>Contact-8463878715</text>
+          </div>
+      </div>
+    </div>
+    <div className='flex items-center justify-center w-full overflow-visible mt-2'>
+      <div className='flex flex-col bg-pink-300 rounded-lg p-4 hover:bg-indigo-300'>
+        <div className='relative w-full h-64 overflow-hidden rounded-lg'>
+          <img
+            src="https://images.pexels.com/photos/28245975/pexels-photo-28245975/free-photo-of-model-posing-at-the-sydney-acc-x-pexels-world-photography-day-meetup-2024.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" // Direct image URL
+            title='img'
+            alt="My first project"
+            className='w-full h-full object-cover'
+            />
+        </div>
+        <div className='mt-4 text-center'>
+          <div className='text-black text-lg font-bold underline font-serif'>
+            Home Delivery Available💦
+          </div>
+          <div className='mt-2 font-bold text-sm text-black font-sans '>
+            New And fresh service 100 rupee
+          </div>
+        </div>
+        <div className='flex bg-white h-8 w-28 justify-center ml-24 mt-4 hover:bg-pink-200 rounded-lg hover:w-32 hover:h-10'>
+          <button className="flex items-center font-serif">
+            BOOK NOW
+          </button>
+        </div>
+          <div className='flex font-serif flex-col left-5 '>
+          <text className='flex underline ml-[163px] mt-4'>Contact-8463878715</text>
+          </div>
+      </div>
+    </div>
+    <div className='flex items-center justify-center w-full overflow-visible mt-2'>
+      <div className='flex flex-col bg-pink-300 rounded-lg p-4 hover:bg-indigo-300'>
+        <div className='relative w-full h-64 overflow-hidden rounded-lg'>
+          <img
+            src="https://images.pexels.com/photos/28223249/pexels-photo-28223249/free-photo-of-a-woman-in-a-bikini-is-splashing-water-out-of-her-hair.jpeg" // Direct image URL
+            title='img'
+            alt="My first project"
+            className='w-full h-full object-cover'
+            />
+        </div>
+        <div className='mt-4 text-center'>
+          <div className='text-black text-lg font-bold underline font-serif'>
+            Home Delivery Available💦
+          </div>
+          <div className='mt-2 font-bold text-sm text-black font-sans '>
+            New And fresh service 250 rupee
+          </div>
+        </div>
+        <div className='flex bg-white h-8 w-28 justify-center ml-24 mt-4 hover:bg-pink-200 rounded-lg hover:w-32 hover:h-10'>
+          <button className="flex items-center font-serif">
+            BOOK NOW
+          </button>
+        </div>
+          <div className='flex font-serif flex-col left-5 '>
+          <text className='flex underline ml-[163px] mt-4'>Contact-8463878715</text>
+          </div>
+      </div>
+    </div>
+            </div>
+  );
+}
+
+export default App;
